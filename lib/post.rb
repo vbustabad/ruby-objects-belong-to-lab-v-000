4586bedae8df3b7274bffafc07d43c32d89ad5b2
Veronica Bustabad <vbustabad@gmail.com>
2018-03-29 23:43:51 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,17 @@
+class Post
+
+  attr_accessor :title, :author, :name
+
+  def title= (title)
+    @title = title
+  end
+
+  def author= (author)
+    @author = author
+  end
+
+  def name= (name)
+    @name = name
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+class Song
+
+  attr_accessor :title, :artist, :name
+
+  def title= (title)
+    @title = title
+  end
+
+  def artist= (artist)
+    @artist = artist
+  end
+
+  def name= (name)
+    @name = name
+  end
+
+end
