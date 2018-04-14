4129b5f8ebe810d5eaa046bd2e259dc6be2d59f2
Veronica Bustabad <vbustabad@gmail.com>
2018-04-14 02:04:38 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,17 +1,3 @@
 class Post
-
   attr_accessor :title, :author, :name
-
-  def title= (title)
-    @title = title
-  end
-
-  def author= (author)
-    @author = author
-  end
-
-  def name= (name)
-    @name = name
-  end
-
 end
